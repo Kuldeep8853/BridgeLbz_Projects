78d40ffd38821c16d232571b5ce3dc7f363819a9
stru8854 <60762201+stru8854@users.noreply.github.com>
2020-04-01 00:31:22 +0530

    [Kuldeep]Add. Multiple Rides

--- Invoice_Generator/Invoice_Generator/CabInvoiceGenerator.cs
+++ Invoice_Generator/Invoice_Generator/CabInvoiceGenerator.cs
@@ -10,10 +10,21 @@ namespace Invoice_Generator
         private const double minimun_Cost_Per_Kilometer = 10;
         private const double minimun_Fare = 5;
 
-        public static double CalaculateFare(double kilometer, int time)
+        public static double CalculateFare(double kilometer, int time)
         {
             double Total_Cast = kilometer * minimun_Cost_Per_Kilometer + time * cost_Per_Minut;
             return Math.Max(Total_Cast, minimun_Fare);
+        }
+
+        public static double CalculateFare(Ride[] rides)
+        {
+            double Total_fare=0;
+            foreach(Ride ride in rides)
+            {
+                Total_fare+=CalculateFare(ride.Distance, ride.Time);
+            }
+
+            return Total_fare;
         }
     }
 }
--- Invoice_Generator/NUnitTestProject/InvoiceGeneratorTests.cs
+++ Invoice_Generator/NUnitTestProject/InvoiceGeneratorTests.cs
@@ -9,7 +9,7 @@ namespace NUnitTestProject
         [Test]
         public void Test_Ride_Fare1()
         {
-            double actual = CabInvoiceGenerator.CalaculateFare(0.1, 2);
+            double actual = CabInvoiceGenerator.CalculateFare(0.1, 2);
             double expected = Math.Max(5, (0.1*10)+(2*1));
             Assert.AreEqual(actual,expected);
         }
@@ -17,8 +17,30 @@ namespace NUnitTestProject
         [Test]
         public void Test_Ride_Fare2()
         {
-            double actual = CabInvoiceGenerator.CalaculateFare(0.5, 7);
-            double expected = Math.Max(5, (0.5 * 10) + (7 * 1)); ;
+            double actual = CabInvoiceGenerator.CalculateFare(0.5, 7);
+            double expected = Math.Max(5, (0.5 * 10) + (7 * 1));
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Test_MultipleRide_Fare1()
+        {
+            Ride[] rides= new Ride[2];
+            rides[0] = new Ride { Distance=2.0, Time=10 };
+            rides[1] = new Ride { Distance = 10.0, Time = 50 };
+            double actual = CabInvoiceGenerator.CalculateFare(rides);
+            double expected = CabInvoiceGenerator.CalculateFare(2.0, 10) + CabInvoiceGenerator.CalculateFare(10.0, 50);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void Test_MultipleRide_Fare2()
+        {
+            Ride[] rides = new Ride[2];
+            rides[0] = new Ride { Distance = 0.5, Time = 5 };
+            rides[1] = new Ride { Distance = 1.0, Time = 8 };
+            double actual = CabInvoiceGenerator.CalculateFare(rides);
+            double expected = CabInvoiceGenerator.CalculateFare(0.5, 5) + CabInvoiceGenerator.CalculateFare(1.0, 8);
             Assert.AreEqual(actual, expected);
         }
     }
